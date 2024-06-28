40ce5f3346646829b384577e717315ddae6d16f4
Lembo <54678184+lembo2003@users.noreply.github.com>
2024-06-28 17:34:41 +0700

    PasswordGenerator
    Whoohoo

---
+++ RandomPasswordGenerator/index.js
@@ -0,0 +1,37 @@
+
+function gen(length, lowerCase, upperCase, number, symbol){
+    const lowercasechars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercasechars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberchars ="0123456789";
+    const symbolchars ="!@#$%^&*()_+-";
+
+    let allowdChars = "";
+    let password = "";
+
+    allowdChars += lowerCase.checked ? lowercasechars : "";
+    allowdChars += upperCase.checked ? uppercasechars : "";
+    allowdChars += number.checked ? numberchars : "";
+    allowdChars += symbol.checked ? symbolchars : "";
+
+    if(length<=0)
+        return `(Password length must be at least 1), now its ${length}`;
+
+    if(allowdChars.length===0){
+        return `Please choose at least option lease`;
+    }
+    
+    for(let i = 0;i<length;i++)
+        {const randomIndex = Math.floor(Math.random() * allowdChars.length);
+        password += allowdChars[randomIndex];}
+    return password;
+}
+function lembo(){
+    const passwordLength =  document.getElementById("length").value;
+    const lowercase = document.getElementById("lowerCase");
+    const uppercase = document.getElementById("upperCase");
+    const number = document.getElementById("number");
+    const symbol = document.getElementById("symbols");
+    console.log(symbol.checked);
+    let result = document.getElementById("gen");
+    result.textContent = gen(passwordLength, lowercase, uppercase, number, symbol);
+}
